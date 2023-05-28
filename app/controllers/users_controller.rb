6e316b26e88a0e0613dcffea1ca07d25e0cf091e
syusaku18 <130909211+syusaku18@users.noreply.github.com>
2023-05-28 10:42:32 +0000

    [finish]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user =User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] ="You have updated book successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+    @book=Book.new
+    @books=@user.books
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to  user_path(current_user)
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root to:'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  devise_for :users
+
+  resources :books, only: [:index, :show,:edit, :create, :destroy, :update]
+  resources :users, only: [:show, :edit, :index, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
